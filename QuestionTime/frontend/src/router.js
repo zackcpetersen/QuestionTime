626b193daf853fc3f8253867530ebe3927cd6ca1
n3rdn1nja <zackcpetersen@gmail.com>
2019-10-24 11:31:17 -0600

    cleaned up code and added content to homepage

--- QuestionTime/frontend/src/router.js
+++ QuestionTime/frontend/src/router.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import Example from "./views/Example.vue";
 
 Vue.use(Router);
 
@@ -14,19 +13,5 @@ export default new Router({
       name: "home",
       component: Home
     },
-      {
-      path: "/example",
-      name: "example",
-      component: Example
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
   ]
 });
