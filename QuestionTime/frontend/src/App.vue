994e4c85a1ee2cf3a1609bcd4ba7896a1085bcf9
n3rdn1nja <zackcpetersen@gmail.com>
2019-10-24 09:19:10 -0600

    started CSRF_TOKEN and formatted files

--- QuestionTime/frontend/src/App.vue
+++ QuestionTime/frontend/src/App.vue
@@ -1,26 +1,28 @@
 <template>
-  <div id="app">
-    <Navbar />
-    <router-view />
-  </div>
+    <div id="app">
+        <Navbar/>
+        <router-view/>
+    </div>
 </template>
 
 <script>
-  import Navbar from "./components/Navbar";
-  export default {
-    name: "App",
-    components: {
-      Navbar
+    import Navbar from "./components/Navbar";
+
+    export default {
+        name: "App",
+        components: {
+            Navbar
+        }
     }
-  }
 </script>
 
 <style>
-html, body {
+    html, body {
         height: 100%;
         font-family: 'Playfair Display', serif;
     }
-  .btn:focus {
-    box-shadow: none !important;
-  }
+
+    .btn:focus {
+        box-shadow: none !important;
+    }
 </style>
--- QuestionTime/frontend/src/components/Navbar.vue
+++ QuestionTime/frontend/src/components/Navbar.vue
@@ -2,31 +2,31 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light my-navbar">
         <div class="container">
             <router-link
-                            :to="{ name: 'home' }"
-                            class="navbar-brand"
-                    >QuestionTime
-                    </router-link>
+                    :to="{ name: 'home' }"
+                    class="navbar-brand"
+            >QuestionTime
+            </router-link>
 
             <div class="collapse navbar-collapse">
-            <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
-                    <router-link
-                            :to="{ name: 'home' }"
-                            class="btn btn-sm btn-success"
-                    >Home
-                    </router-link>
-                </li>
-                <li class="nav-item mx-1">
-                <router-link
-                            :to="{ name: 'about' }"
-                            class="btn btn-sm btn-danger"
-                    >About
-                    </router-link>
-                </li>
-                <li class="nav-item">
-                <a class="btn btn-sm btn-outline-secondary" href="/accounts/logout/">Logout</a>
-                </li>
-            </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item active">
+                        <router-link
+                                :to="{ name: 'home' }"
+                                class="btn btn-sm btn-success"
+                        >Home
+                        </router-link>
+                    </li>
+                    <li class="nav-item mx-1">
+                        <router-link
+                                :to="{ name: 'about' }"
+                                class="btn btn-sm btn-danger"
+                        >About
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-sm btn-outline-secondary" href="/accounts/logout/">Logout</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
@@ -42,10 +42,12 @@
     .my-navbar {
         border-bottom: 1px solid #DDD;
     }
+
     .navbar-brand {
         font-weight: bold;
         font-size: 130%;
     }
+
     .navbar-brand:hover {
         color: #DC3545 !important;
     }
