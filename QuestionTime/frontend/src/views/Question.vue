5b8eaab38be0104e359f6d8305d07b2d0e0377fa
n3rdn1nja <zackcpetersen@gmail.com>
2019-11-23 15:49:11 -0700

    added answer question button and load more answers button

--- QuestionTime/frontend/src/views/Question.vue
+++ QuestionTime/frontend/src/views/Question.vue
@@ -7,6 +7,36 @@
             </p>
             <p>{{ question.created_at }}</p>
             <hr>
+            <div v-if="userHasAnswered">
+                <p class="answer-added">You've written an answer</p>
+            </div>
+            <div v-else-if="showForm">
+                <form class="card" @submit.prevent="onSubmit">
+                    <div class="card-header px-3">
+                        Answer the Question
+                    </div>
+                    <div class="card-block">
+                        <textarea
+                                v-model="newAnswerBody"
+                                class="form-control"
+                                placeholder="Share Your Knowledge!"
+                                rows="5"
+                        ></textarea>
+                    </div>
+                    <div class="card-footer px-3">
+                        <button type="submit" class="btn btn-sm btn-success">Submit Your Answer</button>
+                    </div>
+                </form>
+                <p v-if="error" class="error mt-2">{{ error }}</p>
+            </div>
+            <div v-else>
+                <button
+                        class="btn btn-sm btn-success"
+                        @click="showForm = true"
+                >Answer the Question
+                </button>
+            </div>
+            <hr>
         </div>
         <div class="container">
             <Answer
@@ -14,6 +44,15 @@
                     :answer="answer"
                     :key="index"
             />
+            <div class="my-4">
+                <p v-show="loadingAnswers">...loading...</p>
+                <button
+                        v-show="next"
+                        @click="getQuestionAnswers"
+                        class="btn btn-sm btn-outline-success"
+                >Load More
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -36,7 +75,13 @@
         data() {
             return {
                 question: {},
-                answers: []
+                answers: [],
+                newAnswerBody: null,
+                error: null,
+                userHasAnswered: false,
+                showForm: false,
+                next: null,
+                loadingAnswers: false,
             }
         },
         methods: {
@@ -48,15 +93,43 @@
                 apiService(endpoint)
                     .then(data => {
                         this.question = data;
+                        this.userHasAnswered = data.user_has_answered;
                         this.setPageTitle(data.content);
                     })
             },
             getQuestionAnswers() {
                 let endpoint = `/api/questions/${this.slug}/answers/`;
+                if (this.next) {
+                    endpoint = this.next;
+                }
+                this.loadingAnswers = true;
                 apiService(endpoint)
                     .then(data => {
-                        this.answers = data.results
+                        this.answers.push(...data.results);
+                        this.loadingAnswers = false;
+                        if (data.next) {
+                            this.next = data.next;
+                        } else {
+                            this.next = null;
+                        }
                     })
+            },
+            onSubmit() {
+                if (this.newAnswerBody) {
+                    let endpoint = `/api/questions/${this.slug}/answer/`;
+                    apiService(endpoint, "POST", {body: this.newAnswerBody})
+                        .then(data => {
+                            this.answers.unshift(data)
+                        });
+                    this.newAnswerBody = null;
+                    this.showForm = false;
+                    this.userHasAnswered = true;
+                    if (this.error) {
+                        this.error = null;
+                    }
+                } else {
+                    this.error = 'You can\'t send an empty answer!'
+                }
             }
         },
         created() {
@@ -70,5 +143,15 @@
     .author-name {
         font-weight: bold;
         color: #DC3545;
+    }
+
+    .answer-added {
+        font-weight: bold;
+        color: green;
+    }
+
+    .error {
+        font-weight: bold;
+        color: red;
     }
 </style>
